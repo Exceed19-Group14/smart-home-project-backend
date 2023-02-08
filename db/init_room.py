1a991121e29db5f86ee20a7e89b58286d179e3d3
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2023-02-08 13:24:05 +0700

    fix: Initial DB use own module

--- db/init_room.py
+++ db/init_room.py
@@ -1,8 +1,22 @@
-from mongo import db
-from routes.room import StateEnum, ModeEnum
-db.get_collection("rooms").insert_many([{
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+MONGO_HOST = os.getenv('MONGO_HOST')
+MONGO_DB = os.getenv('MONGO_DB')
+
+client = MongoClient(MONGO_HOST)
+
+try:
+    conn = client.server_info()
+    print("Connected to MongoDB")
+except:
+    print("Unable to connect MongoDB")
+
+client.get_database(MONGO_DB).get_collection("rooms").insert_many([{
     "id": i,
     "brightness_level": 127,
-    "state": StateEnum.close,
-    "mode": ModeEnum.manual
+    "state": 0,
+    "mode": 0
 } for i in range(1, 4)])
