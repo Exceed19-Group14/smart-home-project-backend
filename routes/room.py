37f7f8c730984fc190c009254fe70aa4fafceae0
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2023-02-08 12:15:28 +0700

    feat: Get Room info

--- routes/room.py
+++ routes/room.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, status
 from pydantic import BaseModel
 from db.mongo import db
 from enum import IntEnum
+from typing import List
+
 router = APIRouter(
     prefix='/room'
 )
@@ -12,8 +14,29 @@ class ModeEnum(IntEnum):
     manual = 0
 
 
+class StateEnum(IntEnum):
+    open = 1
+    close = 0
+
+
 class SetAutoModel(BaseModel):
     mode: ModeEnum
+
+
+class RoomModel(BaseModel):
+    id: int
+    state: StateEnum
+    mode: ModeEnum
+    brightness_level: int
+
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=List[RoomModel])
+def get_room(id: int):
+    docs = list(db.get_collection('rooms').find({
+        "id": id
+    }, {"_id": False}))
+
+    return docs
 
 
 @router.put('/{id}/mode', status_code=status.HTTP_204_NO_CONTENT)
