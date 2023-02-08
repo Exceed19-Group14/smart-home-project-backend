0177725bb5c00136c96fd94d0ac727650e55fba2
Hirasawa Ishigawa <hirasawa_au@yahoo.co.jp>
2023-02-08 11:25:06 +0700

    fix: Mongo error correction

--- db/mongo.py
+++ db/mongo.py
@@ -9,4 +9,10 @@ MONGO_DB = getenv('MONGO_DB')
 
 client = MongoClient(MONGO_HOST)
 
+try:
+    conn = client.server_info()
+    print("Connected to MongoDB")
+except:
+    print("Unable to connect MongoDB")
+
 db = client.get_database(MONGO_DB)
